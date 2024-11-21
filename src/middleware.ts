d55eb317641297d6da8aad500f1a8c849c52fb12
dovanviet <dovanviet2109@gmail.com>
2024-11-21 13:45:23 +0700

    middleware f

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
 import { decrypt } from "@/lib/session";
 
 export default async function middleware(req: NextRequest) {
@@ -8,7 +7,7 @@ export default async function middleware(req: NextRequest) {
   const isProtectedRoute = protectedRoutes.includes(currentPath);
 
   if (isProtectedRoute) {
-    let cookie = req.cookies.get("session");
+    const cookie = req.cookies.get("session");
     const session = await decrypt(cookie?.value);
     console.log("middleware session: " + session);
     if (!session?.userId) {
